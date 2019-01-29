e5a4bd01cd2e834bc87db9d7731d4f78c0428c59
Evelyn Regalado <evelyn.16.1994@gmail.com>
2019-01-29 13:01:43 -0500

    Add files via upload

---
+++ unet_fullold.py
@@ -0,0 +1,310 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import random
+import warnings
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+from itertools import chain
+from skimage.io import imread, imshow, imread_collection, concatenate_images
+from skimage.transform import resize
+from skimage.morphology import label
+
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import *
+from keras.layers.core import Dropout, Lambda
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras import backend as K
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
+import tensorflow as tf
+
+# Set some parameters
+IMG_WIDTH = 128
+IMG_HEIGHT = 128
+IMG_CHANNELS = 3
+TRAIN_PATH = '/home/evelyn/Escritorio/unet_kaggle/stage1_trainold/'
+TEST_PATH = '/home/evelyn/Escritorio/unet_kaggle/stage1_testold/'
+
+warnings.filterwarnings('ignore', category=UserWarning, module='skimage')
+seed = 42
+random.seed = seed
+np.random.seed = seed
+epochs=1
+## get train and test id
+train_ids = next(os.walk(TRAIN_PATH))[1]
+test_ids = next(os.walk(TEST_PATH))[1]
+
+## get data
+
+# Get and resize train images and masks
+X_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+Y_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+
+print('Getting and resizing train images and masks ... ')
+sys.stdout.flush()
+for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
+    path = TRAIN_PATH + id_
+    img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]
+    img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+    X_train[n] = img
+    mask = np.zeros((IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+    for mask_file in next(os.walk(path + '/masks/'))[2]:
+        mask_ = imread(path + '/masks/' + mask_file)
+        mask_ = np.expand_dims(resize(mask_, (IMG_HEIGHT, IMG_WIDTH), mode='constant',
+                                      preserve_range=True), axis=-1)
+        mask = np.maximum(mask, mask_)
+    Y_train[n] = mask
+
+# Get and resize test images
+X_test = np.zeros((len(test_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+Y_test = np.zeros((len(test_ids), IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+sizes_test = []
+print('Getting and resizing test images ... ')
+sys.stdout.flush()
+for n, id_ in tqdm(enumerate(test_ids), total=len(test_ids)):
+    path = TEST_PATH + id_
+    img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]
+    sizes_test.append([img.shape[0], img.shape[1]])
+    img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+    X_test[n] = img
+
+    mask = np.zeros((IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+    for mask_file in next(os.walk(path + '/masks/'))[2]:
+        mask_ = imread(path + '/masks/' + mask_file)
+        mask_ = np.expand_dims(resize(mask_, (IMG_HEIGHT, IMG_WIDTH), mode='constant',
+                                      preserve_range=True), axis=-1)
+        mask = np.maximum(mask, mask_)
+    Y_test[n] = mask
+
+print('Done!')
+
+""""""
+## image show
+ix = random.randint(0, len(train_ids))
+"""Imagen Entrenamiento RGB"""
+plt.figure('Imagen de entrenamiento')
+plt.subplot(1,2,1) 
+imshow(X_train[ix])
+#plt.show()
+"""Imagen Entrenamiento Segmentada"""
+#plt.figure('Imagen de entrenamiento segmentada') 
+plt.subplot(1,2,2)
+imshow(np.squeeze(Y_train[ix]))
+plt.show()
+
+## define iou metric
+
+def mean_iou(y_true, y_pred):
+    prec = []
+    for t in np.arange(0.5, 1.0, 0.05):
+        y_pred_ = tf.to_int32(y_pred > t)
+        score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
+        K.get_session().run(tf.local_variables_initializer())
+        with tf.control_dependencies([up_opt]):
+            score = tf.identity(score)
+        prec.append(score)
+    return K.mean(K.stack(prec), axis=0)
+
+## build the unet model with keras
+inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+s = Lambda(lambda x: x / 255) (inputs)
+
+conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(s)
+conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+drop4 = Dropout(0.5)(conv4)
+#pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+#conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
+#conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+#drop5 = Dropout(0.5)(conv5)
+
+#up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
+#merge6 = concatenate([drop4,up6], axis = 3)
+#conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+#conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+
+up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop4))
+merge7 = concatenate([conv3,up7], axis = 3)
+conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+
+up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
+merge8 = concatenate([conv2,up8], axis = 3)
+conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+
+up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
+merge9 = concatenate([conv1,up9], axis = 3)
+conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+
+conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
+
+model = Model(inputs=[inputs], outputs = conv10)
+model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = [mean_iou, 'accuracy'])
+#model.summary() """Modelo RNA-Convolucional"""
+
+## fit the model
+earlystopper = EarlyStopping(patience=5, verbose=1)
+checkpointer = ModelCheckpoint('/home/evelyn/Escritorio/unet_kaggle/model-dsbowl2018-1.h5', verbose=1, save_best_only=True)
+results = model.fit(X_train, Y_train, validation_split=0.1, batch_size=16, epochs=epochs,
+                    callbacks=[earlystopper, checkpointer])
+
+## make predictions
+
+# Predict on train, val and test
+model = load_model('/home/evelyn/Escritorio/unet_kaggle/model-dsbowl2018-1.h5', custom_objects={'mean_iou': mean_iou})
+preds_train = model.predict(X_train[:int(X_train.shape[0]*0.9)], verbose=1)
+preds_val = model.predict(X_train[int(X_train.shape[0]*0.9):], verbose=1)
+preds_test = model.predict(X_test, verbose=1)
+
+# CALCULO DE LA MATRIZ DE CONFUSION
+# CONFUSSION MATRIX
+# Cálculo por cada imagen
+for idx_image in range(0, 2):
+    tmp_pred_tests_t = preds_test[idx_image]
+    ## Y_pred
+    threshold = 0.5
+    preds_test_t = (tmp_pred_tests_t > threshold).astype(np.uint8)
+    y_pred = preds_test_t.reshape(1,128*128)
+
+    ## Y_true
+    tmp_y_test = Y_test[idx_image]
+    y_true = tmp_y_test.astype(np.int8).reshape(1, 128*128)
+
+    cm = confusion_matrix(y_true[0], y_pred[0])
+    print("Matriz de confusión imagen: #", idx_image + 1)
+    print(cm)
+
+# CONFUSSION MATRIX
+# Cálculo por todas las imágenes
+threshold = 0.5
+preds_test_t = (preds_test > threshold).astype(np.uint8)
+y_pred = preds_test_t.reshape(1,2*128*128)
+
+## Y_true
+y_true = Y_test.astype(np.int8).reshape(1, 2*128*128)
+tn, fp, fn, tp = confusion_matrix(y_true[0], y_pred[0]).ravel()
+sen=((tp)/(tp+fn))
+spe=((tn)/(tn+fp))
+acc=((tn+tp)/(tn+tp+fp+fn))
+cm = confusion_matrix(y_true[0], y_pred[0])
+print("Matriz de confusión total: ")
+print(cm)
+#print("valores: ")
+#print(tn)
+#print(fp)
+#print(fn)
+#print(tp)
+#print("Sensibilidad", sen)
+#print("Especificidad", spe)
+#print("Accuracy", acc)
+
+# Graficando la matriz de confusión
+sns.heatmap(cm.T, square=True, annot=True, fmt='d', cbar=False)
+plt.xlabel('Clase verdadera')
+plt.ylabel('Clase predecida')
+plt.title('Matriz de Confusión')
+#plt.show()
+
+# Threshold predictions
+preds_train_t = (preds_train > 0.5).astype(np.uint8)
+preds_val_t = (preds_val > 0.5).astype(np.uint8)
+preds_test_t = (preds_test > 0.5).astype(np.uint8)
+
+# Create list of upsampled test masks
+preds_test_upsampled = []
+for i in range(len(preds_test)):
+    preds_test_upsampled.append(resize(np.squeeze(preds_test[i]),
+                                       (sizes_test[i][0], sizes_test[i][1]),
+                                       mode='constant', preserve_range=True))
+
+ix = random.randint(0, len(preds_test_t))
+print(ix)
+plt.figure('Imagen de preds_test_t') 
+plt.subplot(1,2,1)
+imshow(X_test[int(X_test.shape[0]*0.9):][ix]) 
+imshow(X_test[ix])
+#plt.show()
+plt.subplot(1,2,2) 
+imshow(np.squeeze(Y_test[int(Y_test.shape[0]*0.9):][ix]))
+plt.show()
+
+# Perform a sanity check on some random training samples
+ix = random.randint(0, len(preds_train_t))
+plt.figure('Imagen de preds_train_t') 
+plt.subplot(1,2,1) 
+imshow(X_train[ix])
+#plt.show()
+plt.subplot(1,2,2) 
+imshow(np.squeeze(Y_train[ix]))
+plt.show()
+#imshow(np.squeeze(preds_train_t[ix]))
+#plt.show()
+
+# Perform a sanity check on some random validation samples
+ix = random.randint(0, len(preds_val_t))
+plt.figure('Imagen de preds_val_t')
+plt.subplot(1,2,1) 
+imshow(X_train[int(X_train.shape[0]*0.9):][ix])
+#plt.show()
+plt.subplot(1,2,2) 
+imshow(np.squeeze(Y_train[int(Y_train.shape[0]*0.9):][ix]))
+plt.show()
+#imshow(np.squeeze(preds_val_t[ix]))
+#plt.show()
+
+# Run-length encoding stolen from https://www.kaggle.com/rakhlin/fast-run-length-encoding-python
+def rle_encoding(x):
+    dots = np.where(x.T.flatten() == 1)[0]
+    run_lengths = []
+    prev = -2
+    for b in dots:
+        if (b>prev+1): run_lengths.extend((b + 1, 0))
+        run_lengths[-1] += 1
+        prev = b
+    return run_lengths
+
+def prob_to_rles(x, cutoff=0.5):
+    lab_img = label(x > cutoff)
+    for i in range(1, lab_img.max() + 1):
+        yield rle_encoding(lab_img == i)
+
+new_test_ids = []
+rles = []
+for n, id_ in enumerate(test_ids):
+    rle = list(prob_to_rles(preds_test_upsampled[n]))
+    rles.extend(rle)
+    new_test_ids.extend([id_] * len(rle))
+
+
+# Create submission DataFrame
+sub = pd.DataFrame()
+sub['ImageId'] = new_test_ids
+sub['EncodedPixels'] = pd.Series(rles).apply(lambda x: ' '.join(str(y) for y in x))
+sub.to_csv('/home/evelyn/Escritorio/unet_kaggle/sub-dsbowl2018-1.csv', index=False)
